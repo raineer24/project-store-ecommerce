aeb3e71723006be479bf90dc8d1136fabc3916db
raineer <raineerdelarita@gmail.com>
2025-04-01 12:36:39 +0800

    arcjet config db.js

---
+++ backend/config/db.js
@@ -0,0 +1,4 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+
+dotenv.config();
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log('port', PORT);
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
