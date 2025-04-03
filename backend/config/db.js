a79bda3261d54b622a0b019efcc490a83df7b064
raineer <raineerdelarita@gmail.com>
2025-04-03 18:05:15 +0800

    initDb()

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,3 +2,10 @@ import { neon } from "@neondatabase/serverless";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+console.log('password', PGPASSWORD);
+
+export const sql = neon(
+    `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
+);
--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,42 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
+//import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log('port', PORT);
+console.log("port", PORT);
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
+async function initDB() {
+  try {
+    await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+            )
+        `;
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
 });
