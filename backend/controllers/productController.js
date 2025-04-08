e6d04a6b31f50d8b44151e88770f5c652c5b1390
raineer <raineerdelarita@gmail.com>
2025-04-08 14:47:30 +0800

    async function getProduct() sql

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,21 +1,42 @@
 import { sql } from "../config/db.js";
 
-export const getAllProducts = async(req,res) => {
-    try {
-        const products = await sql`
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await sql`
             SELECT * FROM products
             ORDER BY created_at DESC
         `;
 
-        console.log('fetched products', products);
-        res.status(200).json({ success: true, data: products});
-    } catch (error) {
-        console.log('Error in getProducts function', error);
-        res.status(500).json({success: false, message: 'Internal Server Error'});
-    }
+    console.log("fetched products", products);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in getProducts function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
-export const createProduct = async (req,res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price, image } = req.body;
 
-export const getProduct = async (req,res) => {};
-export const updateProduct = async (req,res) => {};
-export const deleteProduct = async (req,res) => {};
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const newProduct = await sql`
+        INSERT INTO products (name,price,image)
+        VALUES (${name},${price},${image})
+        RETURNING *
+    `;
+
+    res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    console.log('Error in createProduct function', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error'});
+  }
+};
+
+export const getProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
