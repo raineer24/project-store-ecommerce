412663b4b70e7022adac3ec6c9a7168f91156b4d
raineer <raineerdelarita@gmail.com>
2025-04-19 18:08:48 +0800

    request fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -42,7 +42,7 @@ export const getProduct = async (req, res) => {
 
   try {
     const product = await sql`
-        SELECT * FROM products WHERE id={id}
+        SELECT * FROM products WHERE id=${id}
         `;
 
     res.status(200).json({ success: true, data: product[0] });
@@ -60,6 +60,7 @@ export const updateProduct = async (req, res) => {
     const updateProduct = await sql`
             UPDATE products
             SET name=${name}, price=${price}, image=${image}
+            WHERE id=${id}
             RETURNING *
         `;
 
@@ -77,5 +78,23 @@ export const updateProduct = async (req, res) => {
   }
 };
 export const deleteProduct = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+        DELETE FROM products WHERE id=${id} RETURNING *
+      `;
+
+    if (deleteProduct.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({ success: true, data: deleteProduct[0] });
+  } catch (error) {
+    console.log("Error in deleteProduct function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
