fad47a159eaacbcbbf2d3d732044f6c2d92fff05
raineer <raineerdelarita@gmail.com>
2025-04-23 15:50:38 +0800

    server js backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ import path from "path";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
-//import { aj } from "./lib/arcjet.js";
+import { aj } from "./lib/arcjet.js";
+
 
 dotenv.config();
 
@@ -20,6 +21,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1.
+    });
+
+    if(decision.isDenied()) {
+      if(decision.reason.isRateLimit()) {
+        res.status(429).json({error: "Too Many Requests"});
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error:" Bot Access Denied"})
+      } else {
+        res.status(403).json({error: 'Forbidden'});
+      }
+
+      return;
+    }
+
+    //check for spoofed bots
+    if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      
+    }
+
+    next();
+  } catch (error) {
+    console.log('Arcjet error', error);
+    next(error);
+  }
+})
 
 app.use("/api/products", productRoutes);
 
